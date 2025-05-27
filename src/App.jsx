a66beaf7391ef3588b50d4c04146b06104cb9b99
Basudev-Pokharel <Basudev.Pokharel@edu.bc.fi>
2025-05-27 20:50:38 +0300

    done stufffs with json-server

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import PersonList from "./employee/PersonList";
 import Header from "./Header_Footers/Header";
 import Footer from "./Header_Footers/Footer";
 import { Outlet } from "react-router";
 import employees from "./employee/employee_object";
+import axios from "axios";
 
 function App() {
-  const [employeeAll, setEmployees] = useState(employees);
+  const [employeeAll, setEmployees] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/employees").then((response) => {
+      setEmployees(response.data);
+    });
+  }, []);
 
   function onAddEmployee(newEmployee) {
+    axios.post("http://localhost:3001/employees", { ...newEmployee });
     setEmployees((prev) => [...prev, newEmployee]);
   }
 
